4fc553be0b2285fb22d87bf7cce1ea85b0bcec98
Youngjin Jo <moolairdev@gmail.com>
2023-01-04 21:38:05 -0800

    passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1356798395093268',
+        clientSecret: '65ddb8cc2513400210bb002a16f45bdc'
+    }
 }
