4f2b79c8e87be7dde0c5e98bce513076f13b4416
Youngjin Jo <moolairdev@gmail.com>
2023-01-21 15:17:51 -0800

    Assignment4 - Favorite /:dishId Post, Delete complete

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -163,13 +163,6 @@ favoriteRouter.route('/:dishId')
                         }, (err) => next(err));
                 }
             })
-        // favorite.remove({ dishes: req.params.dishId })
-        //     .then((resp) => {
-        //         res.statusCode = 200;
-        //         res.setHeader('Content-Type', 'application/json');
-        //         res.json(resp);
-        //     }, (err) => next(err))
-        //     .catch((err) => next(err));
     });
 
 module.exports = favoriteRouter;
